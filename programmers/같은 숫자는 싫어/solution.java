16a3d3d2fd507e646123a93d8dfe153396f63cee
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-09-24 14:19:21 +0900

    updated at 2023. 9. 24. 오후 2:19:21

--- programmers/같은 숫자는 싫어/solution.java
+++ programmers/같은 숫자는 싫어/solution.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = {};
+        
         ArrayList<Integer> arrList = new ArrayList<Integer>();
         
         int num = -1; //원소가 0~9까지 이므로 영향 안받는 아무 숫자.
@@ -15,7 +15,7 @@ public class Solution {
              }
         }
         
-        answer = new int[arrList.size()]; //answer 리스트 크기 선언.
+        int[] answer = new int[arrList.size()]; //answer 리스트 크기 선언.
         for(int i=0; i<answer.length; i++){
             answer[i] = arrList.get(i); 
         }
