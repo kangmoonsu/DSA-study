2a9b044dcf5e4858d023965c8f51eb69f9975a2d
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-09-28 14:41:17 +0900

    uploaded at 2023. 9. 28. 오후 2:41:18

---
+++ programmers/인덱스 바꾸기/solution.java
@@ -0,0 +1,13 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120895
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+        char a = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = a;
+        
+        return new String(arr);
+    }
+}
