c67dc985d4e494aa119e59f5c063d9f389b6c88d
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-09-28 16:46:08 +0900

    updated at 2023. 9. 28. 오후 4:46:09

--- programmers/최솟값 구하기/solution.sql
+++ programmers/최솟값 구하기/solution.sql
@@ -1,6 +1,9 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59038
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59038?language=oracle
 
 SELECT DATETIME
-FROM ANIMAL_INS
-ORDER BY DATETIME ASC
-LIMIT 1;
+FROM (
+    SELECT DATETIME
+    FROM ANIMAL_INS
+    ORDER BY DATETIME ASC
+)
+WHERE ROWNUM = 1;
