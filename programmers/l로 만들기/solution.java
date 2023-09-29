99984a22c9d6e2e49343de031e399855ab159d28
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-09-29 11:03:39 +0900

    uploaded at 2023. 9. 29. 오전 11:03:39

---
+++ programmers/l로 만들기/solution.java
@@ -0,0 +1,9 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181834
+
+class Solution {
+    public String solution(String myString) {
+        String answer;
+        
+        return answer = myString.replaceAll("[a-k]","l");
+    }
+}
