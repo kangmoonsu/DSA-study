a2e858eb758f57fed9df7cef52b32588efdbfac2
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-09-30 11:42:55 +0900

    uploaded at 2023. 9. 30. 오전 11:42:55

---
+++ programmers/두 수의 연산값 비교하기/solution.java
@@ -0,0 +1,10 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181938
+
+class Solution {
+    public int solution(int a, int b) {
+        String abStr = Integer.toString(a) + Integer.toString(b);
+        int a_b = Integer.valueOf(abStr);
+        
+        return a_b >= 2*a*b ? a_b : 2*a*b;
+    }
+}
