4c20e7906b00d6008fa275b1a29520136cd28f53
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-10-04 16:43:41 +0900

    uploaded at 2023. 10. 4. 오후 4:43:40

---
+++ programmers/이름에 el이 들어가는 동물 찾기/solution.sql
@@ -0,0 +1,7 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59047?language=oracle
+
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,NAME
+FROM ANIMAL_INS
+WHERE LOWER(NAME) LIKE '%el%' AND ANIMAL_TYPE LIKE 'Dog'
+ORDER BY NAME;
