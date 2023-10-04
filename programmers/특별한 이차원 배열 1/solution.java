290d0314d09959ec0f586ea67e250e76cf74e797
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-10-04 14:22:13 +0900

    uploaded at 2023. 10. 4. 오후 2:22:12

---
+++ programmers/특별한 이차원 배열 1/solution.java
@@ -0,0 +1,17 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181833
+
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    answer[i][j] = 1;
+                } else{
+                    answer[i][j] = 0;
+                }
+            }
+        }
+        return answer;
+    }
+}
