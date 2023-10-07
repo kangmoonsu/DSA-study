10bccf11f5539eab3e2391a2515d6f46fe6b245c
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-10-07 15:25:54 +0900

    uploaded at 2023. 10. 7. 오후 3:25:53

---
+++ programmers/문자 리스트를 문자열로 변환하기/solution.java
@@ -0,0 +1,11 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181941
+
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        for(String s : arr){
+            answer+=s;
+        }
+        return answer;
+    }
+}
