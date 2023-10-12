26cf56a791943ec393779a62ddf2a88b8773d695
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-10-12 17:03:11 +0900

    uploaded at 2023. 10. 12. 오후 5:03:11

---
+++ programmers/배열 만들기 3/solution.java
@@ -0,0 +1,21 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181895
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < intervals.length; i++) {
+            for (int j = intervals[i][0]; j <= intervals[i][1]; j++) {
+                list.add(arr[j]);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
