593b9a58ac5dcf90095ef5d73c5e267ecca75d45
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-11-09 23:58:12 +0900

    uploaded at 2023. 11. 9. 오후 11:58:11

---
+++ programmers/A로 B 만들기/solution.java
@@ -0,0 +1,18 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120886
+
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        char[] arr1 = before.toCharArray();
+        char[] arr2 = after.toCharArray();
+        
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        
+        String str1 = new String(arr1);
+        String str2 = new String(arr2);
+        
+        return str1.equals(str2) ? 1 : 0;
+    }
+}
