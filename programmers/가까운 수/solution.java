e55a6423f852f03ba63dcf08e9d3c207a39061db
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-11-15 22:37:46 +0900

    uploaded at 2023. 11. 15. 오후 10:37:45

---
+++ programmers/가까운 수/solution.java
@@ -0,0 +1,22 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120890
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        int diff = 0;
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < array.length; i++) {
+            diff = Math.abs(n - array[i]);
+            if (diff < min) {
+                min = diff;
+                answer = array[i];
+                
+            } else if (diff == min && array[i] < answer) {
+                answer = array[i];
+            }
+        }
+        
+        return answer;
+    }
+}
