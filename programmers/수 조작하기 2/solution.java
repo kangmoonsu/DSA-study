873be43549f23d462e9074b87c9d9c0847ac5790
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-11-30 15:17:19 +0900

    uploaded at 2023. 11. 30. 오후 3:17:19

---
+++ programmers/수 조작하기 2/solution.java
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181925
+
+class Solution {
+    public String solution(int[] numLog) {
+       StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < numLog.length; i++) {
+            switch (numLog[i] - numLog[i - 1]) {
+                case 1 -> sb.append("w");
+                case 10 -> sb.append("d");
+                case -1 -> sb.append("s");
+                case -10 -> sb.append("a");
+            }
+        }
+        return sb.toString();
+    }
+}
