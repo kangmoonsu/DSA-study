5f75ecbc061af6a31cb7a3a135ffe91a25b34bf8
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-12-02 19:07:18 +0900

    uploaded at 2023. 12. 2. 오후 7:07:18

---
+++ programmers/문자열을 정수로 바꾸기/solution.java
@@ -0,0 +1,8 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+class Solution {
+    public int solution(String s) {
+        
+        return Integer.parseInt(s);
+    }
+}
