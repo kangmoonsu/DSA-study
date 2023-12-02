ed210d5336cab518be5407b9743c59f74852eabc
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-12-02 19:04:11 +0900

    uploaded at 2023. 12. 2. 오후 7:04:11

---
+++ programmers/짝수와 홀수/solution.java
@@ -0,0 +1,8 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+class Solution {
+    public String solution(int num) {
+        
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+}
