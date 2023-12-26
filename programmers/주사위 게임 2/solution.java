4dde29e435c1509a0b0a9f763dcfe3f205c40535
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2023-12-26 14:19:08 +0900

    uploaded at 2023. 12. 26. 오후 2:19:07

---
+++ programmers/주사위 게임 2/solution.java
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181930
+
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if (a != b && a != c && b != c) {
+            answer = a + b + c;
+        } else if (a == b && a == c) {
+            answer = (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+        } else {
+            answer = (a + b + c) * (a * a + b * b + c * c);
+        }
+        return answer;
+    }
+}
