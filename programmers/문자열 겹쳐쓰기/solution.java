2bf44ddf26d9a2bd42012ea9dfd5e5ee0f9a10b4
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2024-01-08 10:12:09 +0900

    updated at 2024. 1. 8. 오전 10:12:09

--- programmers/문자열 겹쳐쓰기/solution.java
+++ programmers/문자열 겹쳐쓰기/solution.java
@@ -2,12 +2,8 @@
 
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        char[] c = my_string.toCharArray();
-        int a = 0;
-        for(int i = s; i < s + overwrite_string.length() && a < overwrite_string.length(); i++){
-            c[i] = overwrite_string.charAt(a++);
-        }
-        String answer = String.valueOf(c);
-        return answer;
+        String a = my_string.substring(0, s);
+        String b = my_string.substring(s + overwrite_string.length());
+        return a + overwrite_string + b;
     }
 }
