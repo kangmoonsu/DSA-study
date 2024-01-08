f4b60784b4e422e8840412bb90b0fa7d50d97ed4
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2024-01-08 09:49:24 +0900

    uploaded at 2024. 1. 8. 오전 9:49:24

---
+++ programmers/고양이와 개는 몇 마리 있을까/solution.sql
@@ -0,0 +1,7 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59040
+
+-- 코드를 입력하세요
+SELECT ANIMAL_TYPE,COUNT(*) COUNT
+FROM ANIMAL_INS 
+GROUP BY ANIMAL_TYPE 
+ORDER BY ANIMAL_TYPE;
