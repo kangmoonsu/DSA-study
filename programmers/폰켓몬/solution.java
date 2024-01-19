3880a66d6584bcdf162b89965e8e85e5980f1f5b
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2024-01-19 14:08:40 +0900

    updated at 2024. 1. 19. 오후 2:08:39

--- programmers/폰켓몬/solution.java
+++ programmers/폰켓몬/solution.java
@@ -1,6 +1,6 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/1845
 
-import java.util.HashSet;
+import java.util.*;
 
 public class Solution {
   public int solution(int[] nums) {
