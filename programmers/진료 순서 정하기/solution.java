2ef49b1ce8bdc2747114787038baed8a7d99c9f3
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2024-01-22 09:59:26 +0900

    uploaded at 2024. 1. 22. 오전 9:59:25

---
+++ programmers/진료 순서 정하기/solution.java
@@ -0,0 +1,26 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120835
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] emergency) {
+    	
+        int[] copy = new int[emergency.length];
+        int[] answer = new int[emergency.length];
+        
+        for(int i = 0; i <= emergency.length - 1; i++) {
+            copy[i] = emergency[i];
+        }
+        
+        Arrays.sort(copy);
+        
+        for(int i = 0; i <= emergency.length - 1; i++) {
+            for(int j = 0; j <= emergency.length - 1; j++) {
+                if(copy[i] == emergency[j]) {
+                    answer[j] = emergency.length - i;
+                }
+            }
+        }
+        return answer;
+    }
+}
