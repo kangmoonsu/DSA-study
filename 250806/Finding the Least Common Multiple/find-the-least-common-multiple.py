28dff661e3db1ed1b7946b906fae575dca27a04a
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-06 13:40:50 -0500

    Title: Finding the Least Common Multiple, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250806/Finding the Least Common Multiple/find-the-least-common-multiple.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+
+def find_lcm(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+
+    print(n * m // gcd)
+   
+
+find_lcm(n, m)
