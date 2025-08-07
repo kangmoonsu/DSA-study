fb1fcaa35a32a750801bf04e285ef9dc4cbe05a2
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-07 12:10:37 -0500

    Title: 369 Game Using Functions, Time: 54ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250807/369 Game Using Functions/369-games-using-functions.py
+++ 250807/369 Game Using Functions/369-games-using-functions.py
@@ -3,14 +3,15 @@ a, b = map(int, input().split())
 def cnt(a,b):
     cnt = 0
     for i in range(a,b+1):
-        if i // 10 == 3 or i % 10 == 3:
+        if i % 3 == 0:
+            cnt += 1
+        elif i // 10 == 3 or i % 10 == 3:
             cnt += 1
         elif i // 10 == 6 or i % 10 == 6:
             cnt += 1
         elif i // 10 == 9 or i % 10 == 9:
             cnt += 1
-        elif i % 3 == 0:
-            cnt += 1
+        
     return cnt
 
 print(cnt(a,b))
