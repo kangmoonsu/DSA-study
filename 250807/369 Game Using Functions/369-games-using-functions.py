678f7d020bef26b7fb5a8f2184417dba618f1ec3
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-07 12:29:15 -0500

    Title: 369 Game Using Functions, Time: 78ms, Memory: 17MB, Status: Passed - Codetree

--- 250807/369 Game Using Functions/369-games-using-functions.py
+++ 250807/369 Game Using Functions/369-games-using-functions.py
@@ -1,18 +1,22 @@
 a, b = map(int, input().split())
 
-def cnt(a,b):
-    cnt = 0
-    for i in range(a,b+1):
-        if i % 3 == 0:
-            cnt += 1
-        elif i // 10 == 3 or i % 10 == 3:
-            cnt += 1
-        elif i // 10 == 6 or i % 10 == 6:
-            cnt += 1
-        elif i // 10 == 9 or i % 10 == 9:
-            cnt += 1
-        
-    return cnt
+def contains_digit(num, digits):
+    """숫자가 특정 자릿수들(3, 6, 9) 중 하나를 포함하는지 확인"""
+    while num > 0:
+        if num % 10 in digits:
+            return True
+        num //= 10
+    return False
 
-print(cnt(a,b))
-        
+def cnt(a, b):
+    count = 0
+    target_digits = {3, 6, 9}
+    
+    for i in range(a, b + 1):
+        # 3의 배수이거나 3, 6, 9 중 하나의 자릿수를 포함하면 카운트
+        if i % 3 == 0 or contains_digit(i, target_digits):
+            count += 1
+    
+    return count
+
+print(cnt(a, b))
