496b211d5f204bbedc3eecdf2e140ac27ca9f59b
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-08 13:16:39 -0500

    Title: Determining Whole Numbers Using a Function, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250808/Determining Whole Numbers Using a Function/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,22 @@
+
+a, b = tuple(map(int, input().split()))
+
+
+def judge_number(n):
+    if n % 2 == 0:
+        return False
+    elif n % 10 == 5:
+        return False
+    elif n % 3 == 0 and n % 9 != 0:
+        return False
+    else:
+        return True
+
+
+cnt = 0
+
+for i in range(a, b + 1):
+    if judge_number(i):
+        cnt += 1
+
+print(cnt)
