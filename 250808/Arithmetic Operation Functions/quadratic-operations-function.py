ed842af99fb758e968f1a3d86ca4f9782ffd3304
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-08 13:07:12 -0500

    Title: Arithmetic Operation Functions, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250808/Arithmetic Operation Functions/quadratic-operations-function.py
+++ 250808/Arithmetic Operation Functions/quadratic-operations-function.py
@@ -1,19 +1,32 @@
+# Variable declaration and input:
 a, o, c = input().split()
 a = int(a)
 c = int(c)
-r = 0
 
-def operate(a,o,c):
-    if o == "+":
-      r = a+c
-    elif o == "-":
-        r = a-c
-    elif o == "/":
-        r = a//c
-    elif o == "*":
-        r = a*c
-    else:
-        return False
+# Write a function that returns the sum of two numbers.
+def plus(a, b):
+    return a + b
+
+# Write a function that returns the value of a minus b.
+def minus(a, b):
+    return a - b
+
+# Write a function that returns the product of two numbers.
+def times(a, b):
+    return a * b
+
+# Write a function that returns the remainder of a divided by b.
+def divide(a, b):
+    return a // b
 
 
-print(operate(a,o,c))
+if o == '+':
+    print(a, "+", c, "=", plus(a, c))
+elif o == '-':
+    print(a, "-", c, "=", minus(a, c))
+elif o == '*':
+    print(a, "*", c, "=", times(a, c))
+elif o == '/':
+    print(a, "/", c, "=", divide(a, c))
+else:
+    print("False")
