4467ff9decc42c065ad68c1de20adf974bf895e4
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-09 21:55:11 -0500

    Title: Existence of a Date in 2021, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250809/Existence of a Date in 2021/with-or-without-2021.py
+++ 250809/Existence of a Date in 2021/with-or-without-2021.py
@@ -20,11 +20,13 @@ def year2021(M,D):
             return True
         else:
             return False
+    else:
+        return False
 
 if year2021(M,D):
     print("Yes")
 else:
     print("No")
 # 2
-# 1 3 5 7 8 10 12
 # 4 6 9 11 
+# 1 3 5 7 8 10 12
