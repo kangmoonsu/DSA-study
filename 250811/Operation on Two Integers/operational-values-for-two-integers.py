4ff21745b3091e7d3dfab0f9e0bf8af5297488fa
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-11 12:51:36 -0500

    Title: Operation on Two Integers, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250811/Operation on Two Integers/operational-values-for-two-integers.py
+++ 250811/Operation on Two Integers/operational-values-for-two-integers.py
@@ -1,9 +1,10 @@
 a, b = map(int, input().split())
 
-minn = min(a,b)
-maxn = max(a,b)
+if a > b:
+    a = a + 25
+    b =  b * 2
+else:
+    a = a * 2
+    b = b + 25
 
-minn = minn*2
-maxn = maxn+25
-
-print(minn,maxn)
+print(a,b)
