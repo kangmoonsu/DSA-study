026b5ffeaf6cc1464552517a0ea35fb9cd6665b8
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-11 14:12:23 -0500

    Title: At Least Two Different Alphabets, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250811/At Least Two Different Alphabets/more-than-one-alphabet.py
@@ -0,0 +1,15 @@
+A = input()
+
+def is_more_twoalp(string):
+    leng = len(string)
+    for i in range(leng):
+        if string[i] != string[0]:
+            return True
+    
+    return False
+
+
+if is_more_twoalp(A):
+    print("Yes")
+else:
+    print("No")
