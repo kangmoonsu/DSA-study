a0120a6b21f27b579fb6d14377f6179982cb5a0e
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-18 10:14:01 -0500

    Title: Fibonacci Numbers Using Recursion, Time: 66ms, Memory: 19MB, Status: Passed - Codetree

--- 250818/Fibonacci Numbers Using Recursion/fibonacci-using-recursive-function.py
+++ 250818/Fibonacci Numbers Using Recursion/fibonacci-using-recursive-function.py
@@ -1,10 +1,10 @@
-N = int(input())
+n = int(input())
 
 def fibo(n):
     if n <= 2:
         return 1
-    else:
-        return fibo(n - 1) + fibo(n - 2)
+    
+    return fibo(n - 1) + fibo(n - 2)
 
 
 print(fibo(n))
