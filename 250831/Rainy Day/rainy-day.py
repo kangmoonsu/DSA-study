796b555a1c0134526fe28c35752ed46228435ebc
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-08-31 22:28:56 -0500

    Title: Rainy Day, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250831/Rainy Day/rainy-day.py
+++ 250831/Rainy Day/rainy-day.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 class Info:
-    def __init__(self,date,day,weather):
+    def __init__(self, date, day, weather):
         self.date = date
         self.day = day
         self.weather = weather
@@ -10,15 +10,14 @@ weather_list = []
 
 for _ in range(n):
     d, dy, w = input().split()
-    i = Info(d,dy,w)
+    i = Info(d, dy, w)
     weather_list.append(i)
 
-sorted_date = sorted(weather_list, key=lambda i: i.date)
 
-for j in range(n):
-    if weather_list[j].weather == "Rain":
-        print(weather_list[j].date,weather_list[j].day,weather_list[j].weather)
+sorted_weather = sorted(weather_list, key=lambda i: i.date)
+
+
+for weather_info in sorted_weather:
+    if weather_info.weather == "Rain":
+        print(weather_info.date, weather_info.day, weather_info.weather)
         break
-
-
-# Please write your code here.
