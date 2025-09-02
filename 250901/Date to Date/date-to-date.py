b28488da600c7829e88d234a80535b92611ec412
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-01 21:34:01 -0500

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250901/Date to Date/date-to-date.py
+++ 250901/Date to Date/date-to-date.py
@@ -1,35 +1,20 @@
-# m1, d1, m2, d2 = map(int, input().split())
+m1, d1, m2, d2 = map(int, input().split())
 
-# num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# days = 0
-# for i in range(m1+1,m2):
-#     days += num_of_days[i]
+days = 0
 
-# days += num_of_days[m1] - d1 + 1
-
-# days += d2
-
-# print(days)
-# Variable declaration and input
-m1, d1, m2, d2 = tuple(map(int, input().split()))
-
-
-def num_of_days(m, d):
-    # For calculation convenience, list the number of days in each month.
-    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    total_days = 0
+if m1 == m2:  # 같은 달인 경우
+    days = d2 - d1 + 1
+else:  # 다른 달인 경우
+    # 중간 달들의 일수 합계
+    for i in range(m1 + 1, m2):
+        days += num_of_days[i]
     
-    # From January to (m - 1) month, all days are fully counted.
-    for i in range(1, m):
-        total_days += days[i]
+    # 시작 달에서 남은 일수
+    days += num_of_days[m1] - d1 + 1
     
-    # For the month m, there are exactly d days.
-    total_days += d
-    
-    return total_days    
+    # 끝 달에서의 일수
+    days += d2
 
-
-# Output
-total_days = num_of_days(m2, d2) - num_of_days(m1, d1) + 1
-print(total_days)
+print(days)
