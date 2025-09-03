ea810c2dc6c144bc7fbc1cb2ba0ef122b80f3a76
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-02 22:03:32 -0500

    Title: DateTime to DateTime, Time: 74ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250902/DateTime to DateTime/datetime-to-datetime.py
+++ 250902/DateTime to DateTime/datetime-to-datetime.py
@@ -2,12 +2,11 @@ a, b, c = map(int, input().split())
 
 total = (a - 11)*60*24
 total += (b-11)*60
-
-if b < 11 or (b == 11 and c <= 11):
+if b < 11 or (b == 11 and c < 11):
     total = -1
-elif b == 11:
+else if b == 11:
     total += c-11
-else:
+elif b > 11:
     total += c+49
 
 print(total)    
