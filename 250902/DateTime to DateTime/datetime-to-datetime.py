e08e51e6df8b6ee29501634f344c90b9d8cce7fd
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-02 21:55:10 -0500

    Title: DateTime to DateTime, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250902/DateTime to DateTime/datetime-to-datetime.py
+++ 250902/DateTime to DateTime/datetime-to-datetime.py
@@ -1,9 +1,14 @@
 a, b, c = map(int, input().split())
 
-if b < 11 or (b == 11 and c < 11):
+total = (a - 11)*60*24
+total += (b-11)*60
+
+if b < 11 or (b == 11 and c <= 11):
     total = -1
+elif b == 11:
+    total += c-11
 else:
-    total = (a - 11) * 60 * 24 + (b - 11) * 60 + (c - 11)
+    total += c+49
 
 print(total)    
 
