22583e948bd62143214e5b6788124e92035bf9aa
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-03 12:39:22 -0500

    Title: Determining the Day of the Week, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250903/Determining the Day of the Week/guess-day-of-week.py
@@ -0,0 +1,15 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+
+total = 0
+for i in range (m1,m2):
+    total += num_of_days[i]
+
+total -= d1
+total += d2
+
+date = total//7
+
+print(days[date])
