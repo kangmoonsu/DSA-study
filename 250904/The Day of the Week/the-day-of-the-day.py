2df122f2e65bddd4efcf7000dfcaf290a9f2b284
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-04 22:49:47 -0500

    Title: The Day of the Week, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/The Day of the Week/the-day-of-the-day.py
+++ 250904/The Day of the Week/the-day-of-the-day.py
@@ -1,24 +1,40 @@
+# m1, d1, m2, d2 = map(int, input().split())
+# A = input()
+
+# num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+# def totalDay(m,d):
+#     num = 0
+#     for i in range(1, m):
+#         num += num_of_days[i]
+#     num+=d
+#     return num
+
+# start_day = (totalDay(m1, d1) - 1) % 7
+# elapsed_day = totalDay(m2,d2) - totalDay(m1,d1) + 1
+
+# count = 0
+# for i in range(elapsed_day):
+#     index = (start_day + i) % 7
+#     day = week[index]
+#     if day == A:
+#         count += 1
+
+# print(count)
 m1, d1, m2, d2 = map(int, input().split())
+
 A = input()
 
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+day = ["Mon", 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+diff = sum(num_of_days[1:m2]) + d2 - (sum(num_of_days[1:m1]) + d1)
 
-def totalDay(m,d):
-    num = 0
-    for i in range(1, m):
-        num += num_of_days[i]
-    num+=d
-    return num
+cnt = 0
+number = diff - day.index(A)
 
-start_day = (totalDay(m1, d1) - 1) % 7
-elapsed_day = totalDay(m2,d2) - totalDay(m1,d1) + 1
+cnt = (number + 7) // 7
 
-count = 0
-for i in range(elapsed_day+1):
-    index = (start_day + i) % 7
-    day = week[index]
-    if day == A:
-        count += 1
+print(cnt)
 
-print(count)
