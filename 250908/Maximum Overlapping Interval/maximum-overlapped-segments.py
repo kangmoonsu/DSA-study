4aa92cad0de38b02208df33c4b172e522ae24e01
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-08 10:45:23 -0500

    Title: Maximum Overlapping Interval, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250908/Maximum Overlapping Interval/maximum-overlapped-segments.py
+++ 250908/Maximum Overlapping Interval/maximum-overlapped-segments.py
@@ -1,10 +1,11 @@
 n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 
-slot = [0] * 300
+slot = [0] * 201
 
 for a, b in segments:
-    for i in range(a, b + 1):
+    a, b = a + 100, b + 100
+    for i in range(a, b):
         slot[i] += 1
 
 maxNum = max(slot)
