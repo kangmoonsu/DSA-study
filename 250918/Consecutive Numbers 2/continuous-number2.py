67fe653c8b12184cb429fc3ab5c60c23d6a43991
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-18 21:30:33 -0500

    Title: Consecutive Numbers 2, Time: 84ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250918/Consecutive Numbers 2/continuous-number2.py
+++ 250918/Consecutive Numbers 2/continuous-number2.py
@@ -1,15 +1,14 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
-arr.sort()
+max_cnt = 1
+current_cnt = 1
 
-cnt = 0
-for i in range(len(arr)-1):
-    if len(arr) == 1:
-        cnt = arr[0]
-    elif arr[i] == arr[i+1]:
-        cnt += 1
-    elif arr[i] != 0:
-        cnt = 0
+for i in range(1, len(arr)):
+    if arr[i] == arr[i-1]:
+        current_count += 1
+        max_cnt = max(max_cnt, current_cnt)
+    else:
+        current_cnt = 1
 
-print(cnt)
+print(max_cnt)
