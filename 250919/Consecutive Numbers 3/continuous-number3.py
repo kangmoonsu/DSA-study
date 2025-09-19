401596acfabe1aa96c185f57b23d3ae3104199c4
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-19 16:04:22 -0500

    Title: Consecutive Numbers 3, Time: 59ms, Memory: 16MB, Status: Passed - Codetree

--- 250919/Consecutive Numbers 3/continuous-number3.py
+++ 250919/Consecutive Numbers 3/continuous-number3.py
@@ -1,16 +1,23 @@
 N = int(input())
 arr = [int(input()) for _ in range(N)]
 
+# 1, -1, -1, 1, -1,-1,-1,-1
+
 max_num = 0
+answer = 0
 pos_num = 0
 neg_num = 0
 
 for i in range(N):
-    if arr[i] < 0:
-        neg_num += 1
-        max_num = max(pos_num, neg_num)
-    elif arr[i] > 0:
+    if arr[i] > 0:
         pos_num += 1
-        max_num = max(pos_num, neg_num)
-print(max_num)
+        max_num = max(answer, pos_num)
+        neg_num = 0
+        answer = max(max_num, pos_num)
+    elif arr[i] < 0:
+        neg_num += 1
+        max_num = max(answer, neg_num)
+        pos_num = 0
+        answer = max(max_num, neg_num)
+print(answer)
         
