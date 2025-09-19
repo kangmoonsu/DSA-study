0307dc5917933baf96f060551620315b701d667a
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-18 20:59:14 -0500

    Title: Consecutive Numbers 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250918/Consecutive Numbers 2/continuous-number2.py
+++ 250918/Consecutive Numbers 2/continuous-number2.py
@@ -5,7 +5,9 @@ arr.sort()
 
 cnt = 0
 for i in range(len(arr)-1):
-    if arr[i] == arr[i+1]:
+    if len(arr) == 1:
+        cnt = arr[0]
+    elif arr[i] == arr[i+1]:
         cnt += 1
     elif arr[i] != 0:
         cnt = 0
