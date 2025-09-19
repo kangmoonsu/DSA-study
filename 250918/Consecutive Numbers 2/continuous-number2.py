4807cdac047df970b6f82676d104cbbfbc7490ab
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-18 21:31:03 -0500

    Title: Consecutive Numbers 2, Time: 65ms, Memory: 16MB, Status: Passed - Codetree

--- 250918/Consecutive Numbers 2/continuous-number2.py
+++ 250918/Consecutive Numbers 2/continuous-number2.py
@@ -6,7 +6,7 @@ current_cnt = 1
 
 for i in range(1, len(arr)):
     if arr[i] == arr[i-1]:
-        current_count += 1
+        current_cnt += 1
         max_cnt = max(max_cnt, current_cnt)
     else:
         current_cnt = 1
