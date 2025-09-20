7b917874e2b76d5904f87f0e74c13c22ac590708
Kang Moonsu <131429968+kangmoonsu@users.noreply.github.com>
2025-09-20 16:21:50 -0500

    Title: Consecutive Numbers 4, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250920/Consecutive Numbers 4/continuous-number4.py
+++ 250920/Consecutive Numbers 4/continuous-number4.py
@@ -2,17 +2,16 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-ans = 0
-max_n = 0
+ans = 1
 
-
-
+# 1 5 2 3 5 8 8
+# 4
 for i in range(n-1):
-    if n == 1:
+    if n == 0:
         ans = 1
     elif arr[i] < arr[i+1]:
         ans += 1
     else:
-        max_n = 1
+        ans = 1
 
-print(max(ans,max_n))
+print(ans)
